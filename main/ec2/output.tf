797749c9cada517c8a3fe39e0a0a8d8872a21f5b
tejas wadaskar <tejaswadaskar@tejass-MacBook-Air.local>
2026-02-13 11:21:59 +0530

    new

--- main/ec2/output.tf
+++ main/ec2/output.tf
@@ -5,3 +5,12 @@ output "tejas_public_ip" {
 output "tejas_private_ip" {
     value = aws_instance.tejas.private_ip
 }
+# Inside modules/vpc/outputs.tf
+
+output "subnetA_id" {
+  value = aws_subnet.your_subnet_resource_name.id
+}
+
+output "t_sg" {
+  value = aws_security_group.your_sg_resource_name.id
+}
--- main/vpc/output.tf
+++ main/vpc/output.tf
@@ -1,11 +1,17 @@
 output "vpc_id" {
     value = aws_vpc.t_vpc.id
 }
-output "subnet-id" {
+output "subnet_id" {
     value = aws_subnet.t_sub.id
 
 }
-output "security_group_id" {
-    value = aws_security_group.t_sg.id 
+
+# Inside modules/vpc/outputs.tf
+
+output "t_sg" {
+  value = aws_security_group.your_sg_resource_name.id
 }
 
+output "webserversg_Arn" {
+    value = aws_security_group.webserversg.arn
+}
