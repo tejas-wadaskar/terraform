ffc1cf00a56578bbf7188f9f4baf9a46fbf46d3e
tejas wadaskar <tejaswadaskar@tejass-MacBook-Air.local>
2026-02-13 11:01:44 +0530

    new

--- main/ec2/output.tf
+++ main/ec2/output.tf
@@ -1,7 +1,7 @@
-output "tejas.public_ip" {
+output "tejas_public_ip" {
     value = aws_instance.tejas.public_ip 
 }
 
-output "tejas.private_ip" {
+output "tejas_private_ip" {
     value = aws_instance.tejas.private_ip
 }
